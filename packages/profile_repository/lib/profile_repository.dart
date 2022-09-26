e45a6e074755e8645aab357130eedfef65eee437
hereWasKitus <nekitdragon@gmail.com>
2022-09-26 16:57:03 +0300

    Remove profile repository

--- packages/profile_repository/lib/profile_repository.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library profile_repository;
-
-export 'src/profile_repository.dart';
-
-// TODO: Export any libraries intended for clients of this package.
